796185e560f8c7099fbda2f74d1bb6cc3d2e6a0d
Baesungchul <130073823+Baesungchul@users.noreply.github.com>
2026-04-21 20:32:41 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,4 @@
+const CACHE='ac1004-v2';
+self.addEventListener('install',e=>{self.skipWaiting();});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(n=>n!==CACHE).map(n=>caches.delete(n)))));self.clients.claim();});
+self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).catch(()=>new Response('offline'))));});
